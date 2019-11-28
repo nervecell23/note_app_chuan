012bbee0b3bffefbef0ef24ff91044bd83f079f2
Chuan Cheng <c_chuan23@hotmail.com>
2019-11-28 12:20:15 +0000

    Add method to create and store new note in note-list

--- jasmine/spec/note-list-modelSpec.js
+++ jasmine/spec/note-list-modelSpec.js
@@ -9,11 +9,19 @@ describe("NoteList", ()=>{
   })
 
   describe("#returnNotes", ()=>{
-
     it("returns all the notes stored in the array", ()=>{
       subject = new NoteList();
       result = subject.returnNotes();
       expect(result).toEqual([]);
+    })
+  })
+
+  describe("#createNote", ()=>{
+    it("creates and stores a note object in the array", ()=>{
+      subject = new NoteList();
+      subject.createNote("new message");
+      result = subject.returnNotes()[0].getText();
+      expect(result).toBe("new message");
     })
   })
 
--- jasmine/src/note-list-model.js
+++ jasmine/src/note-list-model.js
@@ -9,6 +9,11 @@
     return this.noteList;
   }
 
+  NoteList.prototype.createNote = function(string){
+    newNote = new Note(string);
+    this.noteList.push(newNote);
+  }
+
   exports.NoteList = NoteList;
 
 })(this)

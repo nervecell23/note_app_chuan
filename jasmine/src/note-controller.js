bf73f41ced37611eb3e26861bd82c7fe0a13b79d
Chuan Cheng <c_chuan23@hotmail.com>
2019-11-29 17:05:45 +0000

    Add controller

--- jasmine/src/note-controller.js
+++ jasmine/src/note-controller.js
@@ -1,3 +1,15 @@
-var text = document.getElementById("app").innerText;
-console.log(text);
-document.getElementById("app").innerText = 'Chuan';
+(function(exports){
+
+  function Controller(noteList){
+    var noteList = noteList;
+    noteList.createNote("Favourite drink: seltzer");
+    this.noteListView = new NoteListView(noteList);
+  }
+
+  Controller.prototype.render = function(){
+      document.getElementById("app").innerHTML = this.noteListView.render();
+  }
+
+  exports.Controller = Controller;
+
+})(this)

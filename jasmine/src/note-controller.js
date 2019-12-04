085cf511d09c30de33e7ffc1b82336bbd1159d84
Chuan Cheng <c_chuan23@hotmail.com>
2019-12-04 18:57:05 +0000

    Link href in note list to corresponding single note

--- jasmine/src/note-controller.js
+++ jasmine/src/note-controller.js
@@ -1,13 +1,26 @@
 (function(exports){
 
   function Controller(noteList){
-    var noteList = noteList;
-    noteList.createNote("Favourite drink: seltzer");
-    this.noteListView = new NoteListView(noteList);
+    this.noteList = noteList;
+    this.noteList.createNote("Favourite drink: seltzer");
+    window.addEventListener("hashchange", ()=>this.displaySingleNote());
   }
 
   Controller.prototype.render = function(){
-      document.getElementById("app").innerHTML = this.noteListView.render();
+      var noteListView = new NoteListView(this.noteList);
+      document.getElementById("app").innerHTML = noteListView.render();
+  }
+
+  Controller.prototype.findNoteId = function(){
+    console.log("abcde");
+    return Number(window.location.hash.split("#notes/")[1]);
+  }
+
+  Controller.prototype.displaySingleNote = function(){
+    var id = this.findNoteId();
+    var singleNote = this.noteList.returnNotes()[id];
+    var singleNoteView = new SingleNoteView(singleNote);
+    document.getElementById("app").innerHTML = singleNoteView.render();
   }
 
   exports.Controller = Controller;
